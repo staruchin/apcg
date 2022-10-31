731fc2358a464ed6aa75eca04fbdb65c4a76c0c9
staruchin <sv.germignon@gmail.com>
2022-11-01 02:30:42 +0900

    Add docker networks, use angular environment var

--- web/iac-generator/src/app/character.service.ts
+++ web/iac-generator/src/app/character.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
 import { Character } from './character';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CharacterService {
-  private characterUrl = 'http://localhost:8081/apcg/api/v1/character';
+  private characterUrl = `http://${environment.apiUrl}:${environment.apiPort}/apcg/api/v1/character`;
 
   constructor(private http: HttpClient) {}
 
--- web/iac-generator/src/environments/environment.prod.ts
+++ web/iac-generator/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'v160-251-105-81.r2vm.static.cnode.io',
+  apiPort: '8081',
 };
